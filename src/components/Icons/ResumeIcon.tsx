9fe2ba208690ebe8142f61f2e6d70bcbf5a548f8
alwar <alwaromoreno@gmail.com>
2023-06-29 00:05:10 +0200

    resources and resume icons

---
+++ src/components/Icons/ResumeIcon.tsx
@@ -0,0 +1,35 @@
+const ResumeIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      aria-hidden='true'
+      role='img'
+      width='100%'
+      height='100%'
+      preserveAspectRatio='xMidYMid meet'
+      viewBox='0 0 24 24'
+    >
+      <g
+        id='IconifyId18837aa66d28facf51106'
+        fill='none'
+        fill-rule='evenodd'
+        stroke='none'
+        stroke-width='1'
+      >
+        <g
+          id='IconifyId18837aa66d28facf51107'
+          fill='currentColor'
+          fill-rule='nonzero'
+        >
+          <path
+            id='IconifyId18837aa66d28facf51108'
+            d='M15 4H6v16h12V7h-3V4ZM6 2h10l4 4v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2Zm2 9h8v2H8v-2Zm0 4h8v2H8v-2Z'
+          ></path>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default ResumeIcon
